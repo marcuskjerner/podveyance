395ac6af42d0e6bd30c35c206b3c1d6855b3d694
markjerner <mk224bs@student.lnu.se>
2020-07-12 13:10:52 +0200

    Move Storage functionality to Podcast component

--- src/common/PodcastStorageHandler.js
+++ src/common/PodcastStorageHandler.js
@@ -1,23 +1,27 @@
 class PodcastStorageHandler {
-import { Podcast } from '@/views/Podcast';
   constructor () {
-    this.storage = document.localStorage
+    this.storage = Window.localStorage
   }
 
   togglePodcast (podcast) {
-    if (this.storage.hasOwnProperty('podcasts')) {
-      const storedPodcasts = JSON.parse(this.storage.getItem('podcasts'))
-      if (storedPodcasts.includes(podcast)) {
-        const index = storedPodcasts.indexOf(podcast)
-        storedPodcasts.slice(index, index)
+    let storedPodcasts = []
+    if (!this.storage.getItem('podcasts')) {
+      this.storage.setItem('podcasts', JSON.parse([]))
+      storedPodcasts = []
+    } else {
+      storedPodcasts = JSON.parse(this.storage.getItem('podcasts'))
+    }
+    
+    if (storedPodcasts.includes(podcast)) {
+      const index = storedPodcasts.indexOf(podcast)
+      storedPodcasts.slice(index, index)
 
-        this.storage.setItem('podcasts', JSON.stringify(storedPodcasts))
-      } else {
-        storedPodcasts.push(podcast)
-        this.storage.setItem('podcasts', JSON.stringify(storedPodcasts))
-      }
+      this.storage.setItem('podcasts', JSON.stringify(storedPodcasts))
+    } else {
+      storedPodcasts.push(podcast)
+      this.storage.setItem('podcasts', JSON.stringify(storedPodcasts))
     }
   }
 }
 
-
+export default PodcastStorageHandler
--- src/views/Podcast.vue
+++ src/views/Podcast.vue
@@ -1,129 +1,195 @@
 <template>
-    <div class="podcast-info">
-      <div class="podcast-info_header">
-        <div class="podcast-info_thumbnail"></div>
-        
-        <div class="podcast-info_buttons">
-          <div></div> <!-- Button Placeholder -->
-          <div></div> <!-- Button Placeholder -->
-          <div class="btn podcast-info_subscribe_button" @click="toggleSubscription()">
-            <font-awesome-icon icon="check-circle" class="podcast-info_subscribe_icon"/>
-          </div>
+  <div class="podcast-info">
+    <div class="podcast-info_header">
+      <div class="podcast-info_thumbnail"></div>
+
+      <div class="podcast-info_buttons">
+        <div></div>
+        <!-- Button Placeholder -->
+        <div></div>
+        <!-- Button Placeholder -->
+        <div
+          class="btn podcast-info_subscribe_button"
+          @click="toggleSubscription()"
+        >
+          <font-awesome-icon
+            icon="check-circle"
+            class="podcast-info_subscribe_icon"
+          />
         </div>
       </div>
-
-      <div v-for="episode in episodes" :key="episode.name" :episodeData="episode" class="episodes">
-        <podcastListItem :episodeData="episode"></podcastListItem>
-      </div>
-
-      <font-awesome-icon icon="chevron-left" class="back-button" @click="$router.go(-1)" style="cursor: pointer;"/>
     </div>
+
+    <div
+      v-for="episode in episodes"
+      :key="episode.name"
+      :episodeData="episode"
+      class="episodes"
+    >
+      <podcastListItem :episodeData="episode"></podcastListItem>
+    </div>
+
+    <font-awesome-icon
+      icon="chevron-left"
+      class="back-button"
+      @click="$router.go(-1)"
+      style="cursor: pointer;"
+    />
+  </div>
 </template>
 
 <script>
-  import RssEpisodeExtractor from '@/common/RssEpisodeExtractor'
-  import PodcastListItem from '@/components/PodcastListItem'
-  const extractor = new RssEpisodeExtractor()
-  export default {
-      name: 'podcast',
-      beforeMount() {
-        this.init()
-        this.getPodcastInfo()
-        this.isSubscribed()
-      },
-      props: {
-        name: String,
-        podcastData: Object,
-      },
-      data: () => {
-        return {
-          episodes: [],
-          isSubscribed: false
-        }
-      },
-      methods: {
-        init() {
-          this.episodes = []
-          this.isSubscribed = false
-        },
-        async getPodcastInfo() {
-          this.episodes = await extractor.getEpisodes(this.$route.params.podcastData.feedUrl)
+import RssEpisodeExtractor from '@/common/RssEpisodeExtractor'
+import PodcastListItem from '@/components/PodcastListItem'
+// import PodcastStorageHandler from '@/common/PodcastStorageHandler'
 
-          const thumbnail = document.querySelector('.podcast-info_thumbnail')
-          thumbnail.style.backgroundImage = `url(${this.$route.params.podcastData.artworkUrl}`
+const extractor = new RssEpisodeExtractor()
+// const PSH = new PodcastStorageHandler()
 
-          console.log(this.episodes)
-        },
-        checkSubscription () {
-          const subsIcon = document.querySelector('.podcast-info_subscribe_icon')
-          
-          if(!this.isSubscribed) {
-            subsIcon.style.color = '#aaa'
-          } else {
-            subsIcon.style.color = '#50C878'
-          }
-        },
-        toggleSubscription() {
-          this.isSubscribed = !this.isSubscribed
-          this.checkSubscription()
-        }
-      },
-      components: {
-        podcastListItem: PodcastListItem
+export default {
+  name: 'podcast',
+  beforeMount () {
+    this.init()
+    this.getPodcastInfo()
+    this.isStored()
+  },
+  mounted () {
+    if (localStorage.getItem('podcasts')) {
+      try {
+        this.myPodcasts = JSON.parse(localStorage.getItem('podcasts'))
+      } catch (e) {
+        localStorage.removeItem('podcasts')
       }
+    }
+  },
+  props: {
+    name: String,
+    podcastData: Object
+  },
+  data: () => {
+    return {
+      episodes: [],
+      isSubscribed: null,
+      myPodcasts: []
+    }
+  },
+  watch: {
+    isSubscribed: {
+      handler: function(val, oldVal) {
+        console.log(val, oldVal)
+        this.checkSubscription(val)
+      },
+      deep: true
+    }
+  }, 
+  methods: {
+    init () {
+      this.episodes = []
+      this.isSubscribed = false
+    },
+    async getPodcastInfo () {
+      this.episodes = await extractor.getEpisodes(
+        this.$route.params.podcastData.feedUrl
+      )
+
+      const thumbnail = document.querySelector('.podcast-info_thumbnail')
+      thumbnail.style.backgroundImage = `url(${this.$route.params.podcastData.artworkUrl}`
+
+      console.log(this.episodes)
+    },
+    checkSubscription (val) {
+      const subsIcon = document.querySelector('.podcast-info_subscribe_icon')
+
+      if (val) {
+        subsIcon.style.color = '#aaa'
+      } else {
+        subsIcon.style.color = '#50C878'
+      }
+    },
+    toggleSubscription () {
+      
+      if (!this.isStored()) {
+        this.myPodcasts.push(this.podcastData)
+        this.storeMyPodcasts()
+      } else {
+        this.unsubscribePodcast()
+        this.storeMyPodcasts()
+      }
+    },
+    isStored () {
+      if (
+        this.myPodcasts.filter(e => e.id === this.podcastData.id).length > 0
+      ) {
+        this.isSubscribed = true
+        return true
+      }
+      this.isSubscribed = false
+      return false
+    },
+    storeMyPodcasts () {
+      const parsedPodcasts = JSON.stringify(this.myPodcasts)
+      localStorage.setItem('podcasts', parsedPodcasts)
+    },
+    unsubscribePodcast () {
+      this.myPodcasts = this.myPodcasts.filter((pod) => pod.id !== this.podcastData.id)
+    }
+  },
+  components: {
+    podcastListItem: PodcastListItem
   }
+}
 </script>
 
 <style lang="scss" scoped>
-  .podcast-info {
-    width: 100%;
-    min-width: 480px;
-    position: relative;
-  }
-  .podcast-info_header {
-      background-color: #0a0a0a;
-      width: 100%;
-      min-height: 240px;
-      padding: 2rem;
-  }
+.podcast-info {
+  width: 100%;
+  min-width: 480px;
+  position: relative;
+}
+.podcast-info_header {
+  background-color: #0a0a0a;
+  width: 100%;
+  min-height: 240px;
+  padding: 2rem;
+}
 
-  .podcast-info_thumbnail {
-    background-size: cover;
-    border-radius: 8px;
-    margin: auto;
-    height: 240px;
-    width: 240px;
-  }
+.podcast-info_thumbnail {
+  background-size: cover;
+  border-radius: 8px;
+  margin: auto;
+  height: 240px;
+  width: 240px;
+}
 
-  .podcast-info_buttons {
-    width: 100%;
-    color: #eee;
-    display: grid;
-    height: 3rem;
-    grid-template-columns: 1fr 1fr 1fr;
-    justify-items: end;
-    align-content: end;
-    .btn {
-      cursor: pointer;
-      color: white;
-      opacity: 0.8;
-      font-size: 1.3em;
+.podcast-info_buttons {
+  width: 100%;
+  color: #eee;
+  display: grid;
+  height: 3rem;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-items: end;
+  align-content: end;
+  .btn {
+    cursor: pointer;
+    color: white;
+    opacity: 0.8;
+    font-size: 1.3em;
 
-      &:hover {
-        opacity: 0.4;
-      }
+    &:hover {
+      opacity: 0.4;
+    }
 
-      &:active {
-        opacity: 1;
-      }
+    &:active {
+      opacity: 1;
     }
   }
+}
 
-  .back-button {
-    position: absolute;
-    left: 1rem;
-    top: 1rem;
-    color: white;
-    font-size: 1.2rem;
-  }
+.back-button {
+  position: absolute;
+  left: 1rem;
+  top: 1rem;
+  color: white;
+  font-size: 1.2rem;
+}
 </style>
